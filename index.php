cb45fd0a9cfcaee0e3623563bcdbccc1e7e77c19
Mattias Bonte <wisecodr@gmail.com>
2020-09-21 16:32:53 +0200

    Add Play Cards

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+var_dump($_POST);
 ?>
 
 <!DOCTYPE html>
@@ -18,14 +19,31 @@ declare(strict_types=1);
   <link rel="stylesheet" type="text/css" href="./src/css/index.css" />
 </head>
 
-<body class="container box-border mx-auto">
-  <header class="text-white bg-black border-gray-400">BLACKJACK</header>
-  <nav class=""></nav>
-  <h1 class=""></h1>
-  <main class="">
-    <article class=""></article>
-    <section class=""></section>
-  </main>
+<body class="container box-border bg-gray-100">
+  <form action="" method="POST">
+    <div class="card">
+      <div class="px-6 py-4">
+        <div class="mb-2 text-xl font-bold">Blackjack</div>
+      </div>
+      <img class="w-full" src="src/img/cards/card-2-clubs.svg">
+      <div class="flex justify-center px-6 pt-4 pb-2">
+
+        <button class="btn" value="draw">
+          <img class="w-8 h-8 mr-2" src="./src/img/draw.svg">
+          <span>Draw</span>
+        </button>
+        <button class="btn" value="hold">
+          <img class="w-8 h-8 mr-2" src="./src/img/stand.svg">
+          <span>Hold</span>
+        </button>
+        <button class="btn" value="stop">
+          <img class="w-8 h-8 mr-2" src="./src/img/stop.svg">
+          <span>Stop</span>
+        </button>
+      </div>
+    </div>
+  </form>
+
   <aside class=""></aside>
   <footer class=""></footer>
 </body>
